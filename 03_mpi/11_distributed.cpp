fcdc6cccb6d902ea3683560f204c44af0130d52f
zhouz <unixjet@hotmail.com>
2024-05-07 13:39:45 +0900

    Assignment 2: update 03_mpi/11_distributed.cpp

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <cmath>
 #include <mpi.h>
 
@@ -31,8 +32,10 @@ int main(int argc, char** argv) {
   for(int irank=0; irank<size; irank++) {
     //MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
     //MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    Body send_buf[N/size];
+    memcpy(send_buf, jbody, sizeof(jbody));
     MPI_Win_fence(0, win);
-    MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);
+    MPI_Put(send_buf, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);
     MPI_Win_fence(0, win);
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
